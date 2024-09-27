27c1027cde1e937064db7450bb37f5858fd9605c
Jason D. Moss <jason@jdmlabs.com>
2024-09-27 07:07:20 -0400

    Update PhpStorm

--- custom/wavebox/default.nix
+++ custom/wavebox/default.nix
@@ -5,12 +5,12 @@
 with lib;
 let
     bits = "x86_64";
-    version = "10.129.28-3";
+    version = "10.129.29-3";
     tarball = "Wavebox_${version}.tar.gz";
 
     src = fetchurl {
         url = "https://download.wavebox.app/beta/linux/tar/${tarball}";
-        sha256 = "sha256-3JMI8TccFpSzhk2GxASbiNDLpG4OCTUJXK/nwaUDXqI=";
+        sha256 = "sha256-TGrzMg33fXU4+g7iWLqqaoWCu1b2aTowwas8q5BXi5g=";
     };
 
     desktopItem = makeDesktopItem rec {
