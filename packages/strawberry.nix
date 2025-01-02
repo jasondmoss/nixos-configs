31fda6ff6b740481c1429fdff0651bfa61d205fd
Jason Moss <jason@cyansolutions.com>
2025-01-01 23:09:40 -0400

    Tweaks

--- packages/strawberry.nix
+++ packages/strawberry.nix
@@ -18,7 +18,8 @@ let
         owner = "jonaski";
         repo = pname;
         rev = version;
-        hash = "sha256-wLC4xL1Dxgwoi6rt33/rpgzErADoU7MKlmvUdkF6Z0s=";
+        hash = "sha256-lmUQSG6JYGU0qQ9G1vuf8Ethp5t30mG20r67bV+dUCw=";  # Icarus
+        #hash = "sha256-wLC4xL1Dxgwoi6rt33/rpgzErADoU7MKlmvUdkF6Z0s=";  # Atreides
     };
 
     meta = with lib; {
