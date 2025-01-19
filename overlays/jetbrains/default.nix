eee7be0164f274542b8e22e524eaa03bb05e30a2
Jason D. Moss <jason@jdmlabs.com>
2025-01-18 19:58:43 -0500

    Development + updates ...

--- overlays/jetbrains/default.nix
+++ overlays/jetbrains/default.nix
@@ -1,14 +1,21 @@
 self: pkgs: with pkgs; {
 
     phpstorm = jetbrains.phpstorm.overrideAttrs (oldAttrs: {
-        version = "2024.3.2";
-        build = "243.23654.115";
+        #version = "2024.3.2";
+        #build = "243.23654.115";
+        #src = fetchurl {
+        #    url = "https://download.jetbrains.com/webide/PhpStorm-2024.3.2.tar.gz";
+        #    sha256 = "sha256-ki8R+He2bRgV1UQLSJFCEmnS0jURLi+mJwthCW2zbC8=";
+        #};
+        version = "2025.1";
+        build = "251.14649.45";
         src = fetchurl {
-            url = "https://download.jetbrains.com/webide/PhpStorm-2024.3.2.tar.gz";
-            sha256 = "sha256-ki8R+He2bRgV1UQLSJFCEmnS0jURLi+mJwthCW2zbC8=";
+            url = "https://download.jetbrains.com/webide/PhpStorm-251.14649.45.tar.gz";
+            sha256 = "sha256-t1kRGJJjYbjF8L1VtU+jl4HE8pOC+rvabCckqgUF458=";
         };
 
-        name = "phpstorm-2024.3.2";
+        #name = "phpstorm-2024.3.2";
+        name = "phpstorm-2025.1";
         wmClass = "jetbrains";
 
         vmopts = ''
