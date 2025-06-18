071cde0739df52ab1674b46c8fabaf5e9b062d27
jasondmoss <jason@jdmlabs.com>
2025-06-18 09:24:49 -0400

    Updates

--- packages/php/default.nix
+++ packages/php/default.nix
@@ -1,4 +1,37 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
+    services.httpd = {
+        enablePHP = true;
+
+        # PHP 8.4
+        phpPackage = pkgs.php84.buildEnv {
+#            extensions = ({ enabled, all }: enabled ++ (with all; [
+#                xdebug
+#            ]));
+
+            extraConfig = ''
+memory_limit=2048M
+xdebug.mode=debug
+            '';
+        };
+
+        phpOptions = ''
+allow_url_fopen = On
+allow_url_include = On
+display_errors = On
+display_startup_errors = On
+max_execution_time = 10000
+max_input_time = 3000
+mbstring.http_input = pass
+mbstring.http_output = pass
+mbstring.internal_encoding = pass
+memory_limit = 2048M
+post_max_size = 2048M
+session.cookie_samesite = "Strict"
+short_open_tag = Off
+upload_max_filesize = 2048M
+        '';
+    };
+
     environment.systemPackages = (with pkgs; [
         php84
     ]) ++ (with pkgs.php84Extensions; [
@@ -21,6 +54,7 @@
         simplexml
         tidy
         tokenizer
+        xdebug
         xml
         xsl
         zip
--- shared/configuration.nix
+++ shared/configuration.nix
@@ -111,6 +111,31 @@ in {
         pcscd.enable = true;
         sysstat.enable = true;
 
+        smartd = {
+            enable = true;
+            autodetect = true;
+#            devices = [
+#                {
+#                    device = "/dev/disk/by-id/nvme-eui.00253856019d735e";
+#                }
+#                {
+#                    device = "/dev/disk/by-id/nvme-eui.0025385a11907ad6";
+#                }
+#                {
+#                    device = "/dev/disk/by-id/ata-WDC_WD4005FZBX-00K5WB0_VBGDTEBF";
+#                }
+#                {
+#                    device = "/dev/disk/by-id/ata-ST4000DM004-2CV104_ZFN32R92";
+#                }
+#                {
+#                    device = "/dev/disk/by-id/ata-WDC_WD4005FZBX-00K5WB0_VBGDTNRF";
+#                }
+#                {
+#                    device = "/dev/disk/by-id/ata-WDC_WD80EFPX-68C4ZN0_WD-RD03LWXE";
+#                }
+#            ];
+        };
+
         coredns = {
             enable = true;
             config = ''
@@ -198,46 +223,43 @@ local {
             user = "me";
             group = "users";
             extraModules = [ "http2" ];
-            enablePHP = true;
 
             # PHP 8.4
-            phpPackage = pkgs.php84.buildEnv {
-                extensions = ({ enabled, all }: enabled ++ (with all; [
-                    xdebug
-                ]));
-
-                extraConfig = ''
-memory_limit=2048M
-xdebug.mode=debug
-                '';
-            };
-
-            phpOptions = ''
-allow_url_fopen = On
-allow_url_include = On
-display_errors = On
-display_startup_errors = On
-max_execution_time = 10000
-max_input_time = 3000
-mbstring.http_input = pass
-mbstring.http_output = pass
-mbstring.internal_encoding = pass
-memory_limit = 2048M
-post_max_size = 2048M
-session.cookie_samesite = "Strict"
-short_open_tag = Off
-upload_max_filesize = 2048M
-            '';
+#            phpPackage = pkgs.php84.buildEnv {
+#                extensions = ({ enabled, all }: enabled ++ (with all; [
+#                    xdebug
+#                ]));
+#
+#                extraConfig = ''
+#memory_limit=2048M
+#xdebug.mode=debug
+#                '';
+#            };
+#
+#            phpOptions = ''
+#allow_url_fopen = On
+#allow_url_include = On
+#display_errors = On
+#display_startup_errors = On
+#max_execution_time = 10000
+#max_input_time = 3000
+#mbstring.http_input = pass
+#mbstring.http_output = pass
+#mbstring.internal_encoding = pass
+#memory_limit = 2048M
+#post_max_size = 2048M
+#session.cookie_samesite = "Strict"
+#short_open_tag = Off
+#upload_max_filesize = 2048M
+#            '';
         };
 
         mysql = {
             enable = true;
             package = pkgs.mariadb;
 
-            settings = {
-                mysqld = {
-                    transaction-isolation = "READ-COMMITTED";
-                };
+            settings.mysqld = {
+                transaction-isolation = "READ-COMMITTED";
             };
 
             ensureUsers = [
