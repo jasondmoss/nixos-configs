7519dbbe9fd403732b4ede82ee9ef1279889b49e
jasondmoss <jason@jdmlabs.com>
2025-07-02 06:43:13 -0400

    Wavebox Update

--- packages/wavebox/default.nix
+++ packages/wavebox/default.nix
@@ -5,12 +5,12 @@
 with lib;
 let
     bits = "x86_64";
-    version = "10.138.5-3";
+    version = "10.138.7-3";
     tarball = "Wavebox_${version}.tar.gz";
 
     src = fetchurl {
         url = "https://download.wavebox.app/beta/linux/tar/${tarball}";
-        sha256 = "sha256-noXW2WGNWfefVxZKH5T2N8KO+pOJUF+uvaMc0BwU0AY=";
+        sha256 = "sha256-06vdopWKV75OugZeeMi1zgro5PkiX/0dghHgsKjvPNc=";
     };
 
     desktopItem = makeDesktopItem rec {
