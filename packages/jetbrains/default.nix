26c58a95ac327de29068bcd0c7a6a0996687629d
jasondmoss <jason@jdmlabs.com>
2025-08-07 07:53:36 -0400

    JetBrains PhpStorm update

--- packages/jetbrains/default.nix
+++ packages/jetbrains/default.nix
@@ -1,13 +1,13 @@
 self: pkgs: with pkgs; {
     phpstorm = jetbrains.phpstorm.overrideAttrs (oldAttrs: {
-        version = "2025.1.4.1";
-        build = "251.27812.24";
+        version = "2025.2";
+        build = "252.23892.419";
         src = fetchurl {
-            url = "https://download.jetbrains.com/webide/PhpStorm-2025.1.4.1.tar.gz";
-            sha256 = "sha256-ks2NVw0DBxiBkHYcfNDQ93NR/xGXNQc8KhAeSjZCkrQ=";
+            url = "https://download.jetbrains.com/webide/PhpStorm-2025.2.tar.gz";
+            sha256 = "sha256-0NPmGq7XxHZbSUeoQQqIXPfhEjxIg0BYXtchUfjAU1A=";
         };
 
-        name = "phpstorm-2025.1.4";
+        name = "phpstorm-2025.2";
         wmClass = "jetbrains";
         vmopts = ''
 -server
@@ -16,22 +16,3 @@ self: pkgs: with pkgs; {
         '';
     });
 }
-
-/*self: pkgs: with pkgs; {
-    phpstorm = jetbrains.phpstorm.overrideAttrs (oldAttrs: {
-        version = "2025.2";
-        build = "252.23892.178";
-        src = fetchurl {
-            url = "https://download.jetbrains.com/webide/PhpStorm-252.23892.178.tar.gz";
-            sha256 = "sha256-85W6EtpQ4BvD+Q2a44P/JZzrm7GDYfo5Ir0HDvwMoAU=";
-        };
-
-        name = "phpstorm-2025.2.EAP";
-        wmClass = "jetbrains";
-        vmopts = ''
--server
--Xms4096m
--Xmx4096m
-        '';
-    });
-}*/
