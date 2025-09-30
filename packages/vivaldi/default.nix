99c3498d1ab56fb48a54a9df4cf835523b3b455a
jasondmoss <jason@jdmlabs.com>
2025-09-30 16:57:21 -0400

    Vivaldi Snapshot update

--- packages/vivaldi/default.nix
+++ packages/vivaldi/default.nix
@@ -25,7 +25,7 @@ let
 in stdenv.mkDerivation rec {
 
     pname = "vivaldi";
-    version = "7.7.3820.3";
+    version = "7.7.3823.3";
 
     suffix = {
         aarch64-linux = "arm64";
@@ -37,7 +37,7 @@ in stdenv.mkDerivation rec {
         # https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_7.5.3725.3-1_amd64.deb
         url = "https://downloads.vivaldi.com/${branch}/vivaldi-${branch}_${version}-1_${suffix}.deb";
         hash = {
-            x86_64-linux = "sha256-c1JSJTK22y1ZkmCHZGZzpKUP3iAzO0OgFqgDtQ/B+ik=";
+            x86_64-linux = "sha256-+TF0hihQQ66+uxwwYuGnQzOPEPkc2Jw5JFi5CJwPsGI=";
         }
         .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
     };
