fb5b4a4ec00badcd11f40287ea48af8252b1a7a4
jasondmoss <jason@jdmlabs.com>
2025-11-21 07:55:33 -0500

    PhpStorm upgrade | Vivaldi SNaphot upgrade

--- packages/jetbrains/default.nix
+++ packages/jetbrains/default.nix
@@ -1,14 +1,15 @@
 self: pkgs: with pkgs; {
     phpstorm = jetbrains.phpstorm.overrideAttrs (oldAttrs: {
-        version = "2025.2.4";
-        build = "252.27397.112";
+        version = "2025.2.5";
+        build = "252.28238.9";
+        wmClass = "jetbrains";
+        name = "phpstorm-2025.2.5";
+
         src = fetchurl {
-            url = "https://download.jetbrains.com/webide/PhpStorm-2025.2.4.tar.gz";
-            sha256 = "sha256-rSQTAGNEdi+0bcRyRumDBTsI92PfXAoroPcO3nmnbD0=";
+            url = "https://download.jetbrains.com/webide/PhpStorm-2025.2.5.tar.gz";
+            sha256 = "sha256-TxAruFrdJSfkZpXzuGbA4xVCPhm8ReCtsN4P5Xeh7+Y=";
         };
 
-        name = "phpstorm-2025.2.4";
-        wmClass = "jetbrains";
         vmopts = ''
 -server
 -Xms6144m
