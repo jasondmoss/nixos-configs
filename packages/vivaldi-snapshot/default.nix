be765263899d0cfe59eb5a37530267c53094ef97
jasondmoss <jason@jdmlabs.com>
2026-01-28 11:13:45 -0500

    Updates

--- packages/vivaldi-snapshot/default.nix
+++ packages/vivaldi-snapshot/default.nix
@@ -22,7 +22,7 @@ let
 
 in stdenv.mkDerivation rec {
     pname = "vivaldi";
-    version = "7.8.3925.45";
+    version = "7.8.3925.53";
 
     suffix = {
         x86_64-linux = "amd64";
@@ -33,7 +33,7 @@ in stdenv.mkDerivation rec {
         # https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_7.7.3845.3-1_amd64.deb
         url = "https://downloads.vivaldi.com/${branch}/vivaldi-${branch}_${version}-1_${suffix}.deb";
         hash = {
-            x86_64-linux = "sha256-H9hHkoP8M+FcXFotjGUEXTi7SMCA+6W10Q/tmj257xs=";
+            x86_64-linux = "sha256-u1i+6XX7gFNaaroUdg+3D4T2Xv6qTiMW7hFxvC5KWrI=";
         }
         .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
     };
--- packages/wavebox-beta/default.nix
+++ packages/wavebox-beta/default.nix
@@ -6,12 +6,12 @@
 with lib;
 let
     bits = "x86_64";
-    version = "10.144.66-3";
+    version = "10.144.72-3";
     tarball = "Wavebox_${version}.tar.gz";
 
     src = fetchurl {
         url = "https://download.wavebox.app/beta/linux/tar/${tarball}";
-        sha256 = "sha256-rAuj0xj1+Cor3hY8X9NAZiYciMLvwU7408wu+SqAFIU=";
+        sha256 = "sha256-W3JvwxeJuZhjxlY7oai6B9bU4XfjKfCgho/RF9Ofkm4=";
     };
 
     desktopItem = makeDesktopItem rec {
