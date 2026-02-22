32e3d018d0adc02e73b7ff0ee142254059fcac70
Jason D. Moss <jason@jdmlabs.com>
2026-02-22 09:09:22 -0500

    Phpstorm update

--- custom-packages/jetbrains/default.nix
+++ custom-packages/jetbrains/default.nix
@@ -1,13 +1,13 @@
 self: pkgs: with pkgs; {
     phpstorm = jetbrains.phpstorm.overrideAttrs (oldAttrs: {
-        version = "2025.3.2";
-        build = "253.30387.85";
+        version = "2025.3.3";
+        build = "253.31033.138";
         wmClass = "jetbrains";
-        name = "phpstorm-2025.3.2";
+        name = "phpstorm-2025.3.3";
 
         src = fetchurl {
-            url = "https://download.jetbrains.com/webide/PhpStorm-2025.3.2.tar.gz";
-            sha256 = "sha256-W9O2DYzJAEtgFb79xYGMUNvi8yMmH+oRhNeoKyHtYO8=";
+            url = "https://download.jetbrains.com/webide/PhpStorm-2025.3.3.tar.gz";
+            sha256 = "sha256-CX7LgeWLKAFYcDHubXqQt9Shz5EVscly/dMwHmz6ht4=";
         };
 
         vmopts = ''
