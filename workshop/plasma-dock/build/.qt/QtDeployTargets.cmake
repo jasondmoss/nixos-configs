f66817fa76d9de8a5702c9394b94838c2489d2c6
Jason D. Moss <jason@jdmlabs.com>
2026-03-18 16:57:46 -0400

    Remove build files

--- workshop/plasma-dock/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_plasma-dock_FILE /home/me/Repository/system/nixos/configs/workshop/plasma-dock/build/plasma-dock)
-set(__QT_DEPLOY_TARGET_plasma-dock_TYPE EXECUTABLE)
--- workshop/plasma-dock/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/me/Repository/system/nixos/configs/workshop/plasma-dock
-# Build directory: /home/me/Repository/system/nixos/configs/workshop/plasma-dock/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
