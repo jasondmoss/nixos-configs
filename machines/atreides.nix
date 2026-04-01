eb39047808a5b7a7c34433f59394aa1de2e2af8f
Jason D. Moss <jason@jdmlabs.com>
2026-04-01 08:17:01 -0400

    Updates

--- machines/atreides.nix
+++ machines/atreides.nix
@@ -16,8 +16,8 @@
     networking.hostName = "atreides";
 
     boot = {
-#        kernelPackages = pkgs.linuxPackages_xanmod_latest;
-        kernelPackages = pkgs.linuxPackages_xanmod;
+        kernelPackages = pkgs.linuxPackages_xanmod_latest;
+#        kernelPackages = pkgs.linuxPackages_xanmod;
 
         initrd = {
             systemd.enable = true;
@@ -153,7 +153,6 @@
         };
 
         nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
-#        nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
     };
 
     programs = {
