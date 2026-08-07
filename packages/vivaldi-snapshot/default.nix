16b58e120a452c3b72c059cdf23d35fab9645550
Jason D. Moss <jason@jdmlabs.com>
2026-08-07 09:31:56 -0400

    Vivaldi update

--- packages/vivaldi-snapshot/default.nix
+++ packages/vivaldi-snapshot/default.nix
@@ -21,7 +21,7 @@ let
     vivaldiName = "vivaldi-snapshot";
 in stdenv.mkDerivation rec {
     pname = "vivaldi";
-    version = "8.2.4116.3";
+    version = "8.2.4126.4";
 
     suffix = {
         x86_64-linux = "amd64";
@@ -31,7 +31,7 @@ in stdenv.mkDerivation rec {
     src = fetchurl {
         url = "https://downloads.vivaldi.com/${branch}/vivaldi-${branch}_${version}-1_${suffix}.deb";
         hash = {
-            x86_64-linux = "sha256-qWbZ29xM3yRSkAYcq2PvjLWDWOtuixo0AkD9Fx3mC+g=";
+            x86_64-linux = "sha256-7dqwAtW8+TMrK7Jjs36eR2G9g8MiOXMi5aLx7THm954=";
         }
         .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
     };
